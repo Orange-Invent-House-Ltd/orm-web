cf753b55a015618bf86d1ef675ea51c63c5be102
Iboro Ekanem <eiboro@oinvent.com>
2025-11-26 03:54:47 +0100

    Add login endpointwith auth

--- src/api/mutation/index.tsx
+++ src/api/mutation/index.tsx
@@ -11,9 +11,20 @@ export const useLogin = () => {
     mutationFn: login,
     onSuccess: (data: any) => {
       // Set auth token and show success message
-      sessionStorage.setItem("token", data.data.token);
-      toast.success(data.message);
-      navigate("/");
+
+      if (
+        (data?.data?.user?.id === 93 &&
+          data?.data?.user?.name === "MYBALANCE APP") ||
+        (data?.data?.user?.name === "MYBALANCEAPP" &&
+          data?.data?.user?.id === 410)
+      ) {
+        sessionStorage.setItem("token", data.data.token);
+        toast.success(data.message);
+        navigate("/");
+      } else {
+          toast.error('Invalid credentials');
+        navigate("/login");
+      }
     },
     onError: (error: any) => {
       const resMessage =
